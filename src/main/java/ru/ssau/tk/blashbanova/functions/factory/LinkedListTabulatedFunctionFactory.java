e32e98496f5e7fa51b69ba321c6cc56a35589819
Blashbanova <blashbanovaa@mail.ru>
2020-11-19 12:14:39 +0400

    3.2 Добавлены методы умножения и сложения, тесты

--- src/main/java/ru/ssau/tk/blashbanova/functions/factory/LinkedListTabulatedFunctionFactory.java
+++ src/main/java/ru/ssau/tk/blashbanova/functions/factory/LinkedListTabulatedFunctionFactory.java
@@ -4,7 +4,6 @@ import ru.ssau.tk.blashbanova.functions.LinkedListTabulatedFunction;
 import ru.ssau.tk.blashbanova.functions.TabulatedFunction;
 
 public class LinkedListTabulatedFunctionFactory implements TabulatedFunctionFactory {
-
     @Override
     public TabulatedFunction create(double[] xValues, double[] yValues) {
         return new LinkedListTabulatedFunction(xValues, yValues);
